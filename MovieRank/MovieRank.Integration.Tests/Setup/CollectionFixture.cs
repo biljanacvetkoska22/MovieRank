2b74525f9dcc0f505b0a38abb1212d12889d7f81
Biljana Cvetkoska <biljana.cvetkoska21@gmail.com>
2020-04-12 01:07:28 +0200

    Init

---
+++ MovieRank/MovieRank.Integration.Tests/Setup/CollectionFixture.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace MovieRank.Integration.Tests.Setup
+{
+    [CollectionDefinition("api")]
+    public class CollectionFixture : 
+        ICollectionFixture<TestContext>
+    {
+    }
+}
---
+++ MovieRank/MovieRank/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MovieRank.Libs.Mappers;
+using MovieRank.Libs.Repositories;
+using MovieRank.Services;
+
+namespace MovieRank
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            if (_env.IsDevelopment())
+            {
+                services.AddSingleton<IAmazonDynamoDB>(cc =>
+                {
+                    var clientConfig = new AmazonDynamoDBConfig { ServiceURL = "http://localhost:8000" };
+                    return new AmazonDynamoDBClient(clientConfig);
+                });
+            }
+            else
+            {
+                services.AddAWSService<IAmazonDynamoDB>();
+                services.AddDefaultAWSOptions(
+                    new AWSOptions
+                    {
+                        Region = RegionEndpoint.GetBySystemName("us-west-2")
+                    });
+            }
+
+            services.AddSingleton<IMovieRankService, MovieRankService>();
+            services.AddSingleton<IMovieRankRepository, MovieRankRepository>();
+            services.AddSingleton<IMapper, Mapper>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
